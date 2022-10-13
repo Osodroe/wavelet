31438ea513a080f4f0afc20cdcd05deb35807ec8
Osodroe <osodroe@gmail.com>
2022-10-12 21:35:18 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,36 +1,44 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.*;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    string[25] library = "";
+    List<String> library = new ArrayList<String>();
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Library size: %d", library.length());
+            String StringOut = String.join(", ", library);
+            return String.format("Current library: %s", StringOut);
+        } 
+        else if (url.getPath().equals("/search")) {
+            System.out.println("Path: " + url.getPath());
+            String[] parameters = url.getQuery().split("=");
+            List<String> buffer = new ArrayList<String>();
+            if (parameters[0].equals("s")) {
+                for (int i = 0; i < library.size(); i += 1)
+                    if(library.get(i).contains(parameters[1]))
+                        buffer.add(library.get(i));
+                String StringOut = String.join(", ", buffer);
+                return String.format("Searched library: %s", StringOut);
+            }
         } 
         else {
             System.out.println("Path: " + url.getPath());
             if (url.getPath().contains("/add")) {
-                String[] search = url.getQuery().split("=");
+                String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("s")) {
-                    return String.format("%s added to library! There are now %d words!", parameters[1], num);;
+                    library.add(parameters[1]);
+                    return String.format("%s added to library!", parameters[1]);
                 }
             }
-            else if (url.getPath().contains("/search")) {
-                String[] search = url.getQuery().split("=");
-                if (parameters[0].equals("s")) {
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
-                }
-                return library;
-            }
-            return "404 Not Found!";
         }
+        return "404 Not Found!";
     }
 }
 
-class NumberServer {
+class SearchEngine {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
